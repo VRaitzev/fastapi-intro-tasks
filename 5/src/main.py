150b02e22a752565316a8ba76b4cb424307b1dd7
VRaitzev <163679361+VRaitzev@users.noreply.github.com>
2024-12-30 17:48:29 +0300

    Update main.py

--- 5/src/main.py
+++ 5/src/main.py
@@ -6,7 +6,6 @@ app = FastAPI()
 # BEGIN (write your solution here)
 @app.post("/users", status_code=200)
 async def create_task(username:str = Body(...), email:str = Body(...), age:Optional[int] = Body(None)):
-    # Создание новой задачи
     new_user = {
         "username": username,
         "email": email,
